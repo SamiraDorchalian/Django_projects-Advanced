0666f57fdae029f711cde1c6c24f807e1555a2e0
samira dorchalian <samiradorchalain1998@gmail.com>
2025-03-09 15:34:55 +0330

    add crispy froms with bootstrap

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,5 @@ from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'registration/signup/signup.html'
+    template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',  # Forgetting this was probably your error
+
     'accounts',
     'pages',
 ]
@@ -129,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy form settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
