1ec38157362295731fc92d33c393e950c6528c3b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-03-11 12:41:53 +0330

    change templates

--- config/settings.py
+++ config/settings.py
@@ -124,8 +124,15 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = "samiradorchalain1998@gmail.com"
+SERVER_EMAIL = "samiradorchalain1998@gmail.com"
+EMAIL_HOST_USER = "samiradorchalain1998@gmail.com"
+EMAIL_HOST_PASSWORD = "1998samira"
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
